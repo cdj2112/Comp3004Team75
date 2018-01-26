a22cbe52884db5754d06686376a1f2a13d42c0f0
Cooper Jones <cooperjones@cmail.carleton.ca>
2018-01-25 19:26:49 -0500

    add ui class and update when player draws a card

--- GameMain/src/main/java/com/QuestCardGame/GameMain/QuestCardGame.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/QuestCardGame.java
@@ -1,25 +1,17 @@
 package com.QuestCardGame.GameMain;
 
-import java.io.FileInputStream;
-
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-/**
- * Hello world!
- *
- */
 public class QuestCardGame extends Application
 {
-	public void start(Stage stage) {
-		Group root = new Group();
-		Scene scene = new Scene(root, 700, 700);
-		
+	public void start(Stage stage) throws Exception {
 		Game game = new Game();
+		game.playerDraw();
+		Group root = new QuestUI(game);
+		Scene scene = new Scene(root, 700, 700);
 		
 		stage.setScene(scene);
 		stage.setTitle("Click and Drag");
