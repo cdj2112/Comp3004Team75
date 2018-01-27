aa2631a3bb52e4e6bdae5586b7e36618273fb82e
Unknown <steffentsmith@gmail.com>
2018-01-27 18:34:04 -0500

    Add a small story deck

--- GameMain/src/main/java/com/QuestCardGame/GameMain/Game.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/Game.java
@@ -3,19 +3,26 @@ package com.QuestCardGame.GameMain;
 public class Game {
 
 	private Player player;
-	private Deck deck;
+	private Deck storyDeck;
 	
 	Game(){
 		player = new Player();
-		deck = new Deck(10);
+		initStoryDeck();
 	}
 	
-	public void playerDraw() {
-		Card c = deck.drawCard();
-		player.drawCard(c);
+	public void playerDraw(Player p, Deck d) {
+		p.drawCard(d.drawCard());
 	}
 	
 	public Player getPlayer() {
 		return player;
+	}
+	
+	private void initStoryDeck() {
+		storyDeck = new Deck();
+		
+		storyDeck.addCard(new QuestCard("Journey Through the Enchanted Forest", 3));
+		storyDeck.addCard(new QuestCard("Vanquish King Arthur's Enemies", 3));
+		storyDeck.addCard(new QuestCard("Repel the Saxon Raiders", 2));		
 	}
 }
