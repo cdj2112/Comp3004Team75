53423fed05bca1b4aa095905276027089ec34bf8
Unknown <steffentsmith@gmail.com>
2018-01-29 16:54:44 -0500

    Add number of cards presently in the deck

--- GameMain/src/main/java/com/QuestCardGame/GameMain/Deck.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/Deck.java
@@ -6,23 +6,32 @@ import java.util.Stack;
 public class Deck {
 
 	private Stack<Card> cards;
+	private int numCards;
 
 	Deck() {
 		cards = new Stack<Card>();
+		numCards = 0;
 	}
 
 	public Card drawCard() {
-		if (cards.size() > 0)
+		if (cards.size() > 0) {
+			numCards--;
 			return cards.pop();
+		}
 		else
 			return null;
 	}
 	
 	public void addCard(Card c) {
 		cards.push(c);
+		numCards++;
 	}
 
 	public void shuffleDeck() {
 		Collections.shuffle(cards);
+	}
+	
+	public int getNumCards() {
+		return numCards;
 	}
 }
