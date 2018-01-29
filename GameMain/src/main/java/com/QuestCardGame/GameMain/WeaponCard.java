049156ec516ac15abc20ad732f72d18f097b4179
Cooper Jones <cooperjones@cmail.carleton.ca>
2018-01-29 17:54:21 -0500

    add basic weapon card class

---
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/WeaponCard.java
@@ -0,0 +1,13 @@
+package com.QuestCardGame.GameMain;
+
+public class WeaponCard extends Card {
+
+	private String name;
+	private int battlePoints;
+
+	WeaponCard(String n, int bp) {
+		super();
+		name = n;
+		battlePoints = bp;
+	}
+}
