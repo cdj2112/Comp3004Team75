5ce34b2f00e59da787d2110ed2eb54fdf76736c7
DishySun <31806074+DishySun@users.noreply.github.com>
2018-02-01 22:23:49 -0500

    Created Amours class

---
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/Amours.java
@@ -0,0 +1,15 @@
+package com.QuestCardGame.GameMain;
+
+public class Amours extends AdventureCard{
+  private int battlePoint;
+  private int bid;
+
+  public Amours(){
+    super("Amours", AdventureType.AMOURS);
+    battlePoint = 10;
+    bid = 1;
+  }
+
+  public int getBid() {return bid;}
+  public int getBattlePoint() {return battlePoint;}
+}
