4a4b4505dc338e67b7b75e9c0ff8b544733769a6
DishySun <31806074+DishySun@users.noreply.github.com>
2018-02-01 20:17:05 -0500

    Created Foe class

---
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/Foe.java
@@ -0,0 +1,32 @@
+package com.QuestCardGame.GameMain;
+
+public class Foe extends AdventureCard{
+
+  private int battlePoint1;
+  private int battlePoint2;
+
+  public Foe(String name, int bp){
+    super(name, AdventureType.FOE);
+    battlePoint1 = battlePoint2 = bp;
+  }
+
+  public Foe(String name, int bp1, int bp2){
+    super(name, AdventureType.FOE);
+
+    //the greater number is signed to battlePoint2
+    if(bp1 > bp2){
+      battlePoint2 = bp1;
+      battlePoint1 = bp2;
+    }else {
+      battlePoint1 = bp1;
+      battlePoint2 = bp2;
+    }
+
+  }
+
+  public int getBattlePoint(boolean questTarget){
+    if (questTarget || battlePoint1 == battlePoint2) return battlePoint2;
+    else return battlePoint1;
+  }
+
+}
