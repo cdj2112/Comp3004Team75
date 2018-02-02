00d7bc21145d9e46a82358005fa5a1b59e492e41
DishySun <31806074+DishySun@users.noreply.github.com>
2018-02-01 20:10:22 -0500

    make the adventure card abstract

--- GameMain/src/main/java/com/QuestCardGame/GameMain/AdventureCard.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/AdventureCard.java
@@ -1,34 +1,13 @@
 package com.QuestCardGame.GameMain;
 
 public class AdventureCard extends Card{
-  private int battlePoint;
-  private int battlePoint2;
   public enum AdventureType {WEAPON, FOE, ALLY, AMOUR,TEST};
   private AdventureType adventureType;
 
-  public AdventureCard(String name){
-
+  public AdventureCard(String name, AdventureType at){
       super(name);
-      battlePoint = battlePoint2 = 0;
-      adventureType = AdventureType.TEST;
+      adventureType = at;
   }
-
-  public AdventureCard(String name, int bp, AdventureType at){
-    super(name);
-    battlePoint = battlePoint2 = bp;
-    adventureType = at;
-  }
-
-  public AdventureCard(String name, int bp, int bp2, AdventureType at){
-    super(name);
-    battlePoint = bp;
-    battlePoint2 = bp2;
-    adventureType = at;
-  }
-
+  
   public AdventureType getCardType(){return adventureType;}
-  public int getBP(){return battlePoint;}
-
-
-
 }
