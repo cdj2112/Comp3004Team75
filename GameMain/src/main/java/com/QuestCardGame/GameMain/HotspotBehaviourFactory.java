19e23da216b0c10ecf5c1193bac1248a8774a6ef
Cooper Jones <cooperjones@cmail.carleton.ca>
2018-02-05 20:06:21 -0500

    hotspot behavior checks action was valid before reparenting

--- GameMain/src/main/java/com/QuestCardGame/GameMain/HotspotBehaviourFactory.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/HotspotBehaviourFactory.java
@@ -4,36 +4,40 @@ import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.input.MouseEvent;
 
-public class HotspotBehaviourFactory{
-  
+public class HotspotBehaviourFactory {
+
 	private Game game;
 	private QuestUI gameUI;
-	
-	HotspotBehaviourFactory(Game g, QuestUI ui){
+
+	HotspotBehaviourFactory(Game g, QuestUI ui) {
 		game = g;
 		gameUI = ui;
 	}
-	
-	public final HotspotBehaviour playCard = new HotspotBehaviour(){
+
+	public final HotspotBehaviour playCard = new HotspotBehaviour() {
 		public void onHit(Card c) {
 			int active = game.activePlayer();
-			game.getPlayer(active).playCard((AdventureCard)c);			
-			Group cardGroup = gameUI.findCardGroup(c);
-			gameUI.getPlayerGroup(active).playCard(cardGroup);
-			EventHandler<MouseEvent> drag = gameUI.findCardListener(c);
-			cardGroup.removeEventHandler(MouseEvent.MOUSE_DRAGGED, drag);
+			boolean play = game.playerPlayCard(game.getPlayer(active), (AdventureCard) c);
+			if (play) {
+				Group cardGroup = gameUI.findCardGroup(c);
+				gameUI.getPlayerGroup(active).playCard(cardGroup);
+				EventHandler<MouseEvent> drag = gameUI.findCardListener(c);
+				cardGroup.removeEventHandler(MouseEvent.MOUSE_DRAGGED, drag);
+			}
 		}
 	};
-	
+
 	public Object getPlayToStage(final int s) {
 		return new HotspotBehaviour() {
 			public void onHit(Card c) {
-				game.sponsorAddCardToStage((AdventureCard)c, s);
-				Group cardGroup = gameUI.findCardGroup(c);
-				gameUI.getPlayerGroup(game.activePlayer()).removeCardFromHand(cardGroup);
-				gameUI.getStageGroup(s).addCardGroup(cardGroup);
-				EventHandler<MouseEvent> drag = gameUI.findCardListener(c);
-				cardGroup.removeEventHandler(MouseEvent.MOUSE_DRAGGED, drag);
+				boolean play = game.sponsorAddCardToStage((AdventureCard) c, s);
+				if (play) {
+					Group cardGroup = gameUI.findCardGroup(c);
+					gameUI.getPlayerGroup(game.activePlayer()).removeCardFromHand(cardGroup);
+					gameUI.getStageGroup(s).addCardGroup(cardGroup);
+					EventHandler<MouseEvent> drag = gameUI.findCardListener(c);
+					cardGroup.removeEventHandler(MouseEvent.MOUSE_DRAGGED, drag);
+				}
 			}
 		};
 	}
