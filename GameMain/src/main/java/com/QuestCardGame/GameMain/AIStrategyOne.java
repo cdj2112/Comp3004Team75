767a64c559b986b9ee99536dbc3b479824a560d3
Unknown <steffentsmith@gmail.com>
2018-02-21 21:32:20 -0500

    Add AI Strategy One framework. No implementation.

---
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/AIStrategyOne.java
@@ -0,0 +1,48 @@
+package com.QuestCardGame.GameMain;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class AIStrategyOne implements AIPlayerStrategy {
+
+	public boolean doIJoinTournament(Game g) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public ArrayList<AdventureCard> playCardsForTournament(Player current) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean doISponsorAQuest(Game g) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Map<Integer, ArrayList<AdventureCard>> createQuest(Player current, QuestCard qc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean doIJoinQuest(Player current, int numStages) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public ArrayList<AdventureCard> playCardsForQuestStage(Game g) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getBidForTest(Game g) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public ArrayList<AdventureCard> discardAfterWinningTest(Player current) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
