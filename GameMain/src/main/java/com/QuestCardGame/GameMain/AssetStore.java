dbf22848af8292c816ec49dc17b45ef825d20bc2
Cooper Jones <cooperjones@cmail.carleton.ca>
2018-02-25 15:05:43 -0500

    move alot of card management to assetstore

--- GameMain/src/main/java/com/QuestCardGame/GameMain/AssetStore.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/AssetStore.java
@@ -2,19 +2,65 @@ package com.QuestCardGame.GameMain;
 
 import java.io.FileInputStream;
 import java.util.HashMap;
+
+import javafx.event.EventHandler;
+import javafx.geometry.Point2D;
+import javafx.scene.Group;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 
 public class AssetStore {
-	public static HashMap<Integer, CardGroup> cardGroups = new HashMap<Integer, CardGroup>();
+	public HashMap<Integer, CardGroup> cardGroups = new HashMap<Integer, CardGroup>();
+	private HashMap<Integer, EventHandler<MouseEvent>> dragListener = new HashMap<Integer, EventHandler<MouseEvent>>();
+	
 	public static HashMap<String, Image> imageStore = new HashMap<String, Image>();
+	
+	private QuestUI UI;
+	
+	public AssetStore(QuestUI qUI) {
+		UI = qUI;
+	}
 
-	private static CardGroup makeNewCardGroup(Card c) {
+	private CardGroup makeNewCardGroup(Card c) {
 		CardGroup cg = new CardGroup(c);
+		
+		final Card eventCard = c;
+		final Group eventGroup = cg;
+		EventHandler<MouseEvent> dragged = new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent e) {
+				UI.setDraggingCard(eventCard);
+				double x = e.getX();
+				double y = e.getY();
+				Point2D p = eventGroup.localToParent(x, y);
+				eventGroup.setTranslateX(p.getX() - 50);
+				eventGroup.setTranslateY(p.getY() - 50);
+				UI.update();
+			}
+		};
+		EventHandler<MouseEvent> mouseUp = new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent e) {
+				if (UI.getDraggingCard() != eventCard) {
+					return;
+				}
+				UI.dropCard(e.getSceneX(), e.getSceneY());
+				System.out.println("Card Up");
+				UI.setDraggingCard(null);
+				UI.update();
+			}
+		};
+
+		cg.addEventHandler(MouseEvent.MOUSE_DRAGGED, dragged);
+		cg.addEventHandler(MouseEvent.MOUSE_RELEASED, mouseUp);
+		dragListener.put(c.getId(), dragged);
 		cardGroups.put(c.getId(), cg);
 		return cg;
 	}
+	
+	public EventHandler<MouseEvent> getCardListener(Card c){
+		return dragListener.get(c.getId());
+	}
 
-	public static CardGroup getCardGroup(Card c) {
+	public CardGroup getCardGroup(Card c) {
 		CardGroup cg = cardGroups.get(c.getId());
 		if (cg != null) {
 			return cg;
--- GameMain/src/main/java/com/QuestCardGame/GameMain/QuestUI.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/QuestUI.java
@@ -28,9 +28,8 @@ public class QuestUI extends Group {
 
 	private Game game;
 	private HotspotBehaviourFactory behaviourFactory;
+	private AssetStore assetStore;
 
-	private HashMap<Integer, Group> cardAssets;
-	private HashMap<Integer, EventHandler<MouseEvent>> dragListener;
 	private Card draggingCard;
 
 	private Hotspot[] stageHotspots;
@@ -56,10 +55,9 @@ public class QuestUI extends Group {
 		game = g;
 		int numPlayers = game.getNumPlayers();
 
-		cardAssets = new HashMap<Integer, Group>();
+		assetStore = new AssetStore(this);
 		stageHotspots = new Hotspot[5];
 		playHotspots = new Hotspot[numPlayers];
-		dragListener = new HashMap<Integer, EventHandler<MouseEvent>>();
 		dialogListeners = new HashMap<String, EventHandler<ActionEvent>>();
 		behaviourFactory = new HotspotBehaviourFactory(game, this);
 
@@ -186,56 +184,14 @@ public class QuestUI extends Group {
 		update();
 	}
 
-	private void dropCard(double x, double y) {
+	public void dropCard(double x, double y) {
 		for (Hotspot h : stageHotspots) {
 			h.checkColision(draggingCard, x, y);
 		}
 		playHotspots[game.getCurrentActivePlayer()].checkColision(draggingCard, x, y);
 		discardHotspot.checkColision(draggingCard, x, y);
 	}
-
-	private Group makeNewCardGroup(Card c) {
-		Group g = new Group();
-		Rectangle r = new Rectangle();
-		r.setHeight(100);
-		r.setWidth(100);
-		r.setFill(Color.RED);
-		Text t = new Text(40, 40, "" + c.getName());
-		t.setFont(new Font(20));
-		g.getChildren().addAll(r, t);
-
-		final Card eventCard = c;
-		final Group eventGroup = g;
-		EventHandler<MouseEvent> dragged = new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent e) {
-				draggingCard = eventCard;
-				double x = e.getX();
-				double y = e.getY();
-				Point2D p = eventGroup.localToParent(x, y);
-				eventGroup.setTranslateX(p.getX() - 50);
-				eventGroup.setTranslateY(p.getY() - 50);
-				update();
-			}
-		};
-		EventHandler<MouseEvent> mouseUp = new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent e) {
-				if (draggingCard != eventCard) {
-					return;
-				}
-				dropCard(e.getSceneX(), e.getSceneY());
-				System.out.println("Card Up");
-				draggingCard = null;
-				update();
-			}
-		};
-
-		g.addEventHandler(MouseEvent.MOUSE_DRAGGED, dragged);
-		dragListener.put(c.getId(), dragged);
-		g.addEventHandler(MouseEvent.MOUSE_RELEASED, mouseUp);
-
-		return g;
-	}
-
+	
 	private void repositionCards() {
 		for (int i = 0; i < game.getNumPlayers(); i++) {
 			Player p = game.getPlayer(i);
@@ -244,7 +200,7 @@ public class QuestUI extends Group {
 			for (Card c : pHand) {
 				if (c == draggingCard)
 					continue;
-				CardGroup g = AssetStore.getCardGroup(c);
+				CardGroup g = assetStore.getCardGroup(c);
 				if(!playerGroups[i].getHand().getChildren().contains(g)) {
 					playerGroups[i].addCardToHand(g);
 				}
@@ -258,7 +214,7 @@ public class QuestUI extends Group {
 			for (Card c : pPlay) {
 				if (c == draggingCard)
 					continue;
-				CardGroup g = AssetStore.getCardGroup(c);
+				CardGroup g = assetStore.getCardGroup(c);
 				if(!playerGroups[i].getPlay().getChildren().contains(g)) {
 					playerGroups[i].playCard(g);
 				}
@@ -274,7 +230,7 @@ public class QuestUI extends Group {
 			for (Stage s : q.getStages()) {
 				int xOffset = 0;
 				for (Card c : s.getCards()) {
-					CardGroup g = AssetStore.getCardGroup(c);
+					CardGroup g = assetStore.getCardGroup(c);
 					g.setTranslateX(xOffset * 110.0);
 					g.setTranslateY(0);
 					xOffset++;
@@ -369,8 +325,7 @@ public class QuestUI extends Group {
 						Platform.runLater(new Runnable() {
 							public void run() {
 								for (AdventureCard c : discard) {
-									int id = c.getId();
-									Group g = cardAssets.get(id);
+									Group g = assetStore.getCardGroup(c);
 									Group p = (Group) g.getParent();
 									if (p != null) {
 										p.getChildren().remove(g);
@@ -400,12 +355,16 @@ public class QuestUI extends Group {
 		}
 	}
 
-	public Group findCardGroup(Card c) {
-		return cardAssets.get(c.getId());
+	public Card getDraggingCard() {
+		return draggingCard;
+	}
+	
+	public void setDraggingCard(Card c) {
+		draggingCard = c;
 	}
 
 	public EventHandler<MouseEvent> findCardListener(Card c) {
-		return dragListener.get(c.getId());
+		return assetStore.getCardListener(c);
 	}
 
 	public PlayerGroup getPlayerGroup(int i) {
@@ -414,5 +373,9 @@ public class QuestUI extends Group {
 
 	public StageGroup getStageGroup(int i) {
 		return stageGroups[i];
+	}
+	
+	public CardGroup findCardGroup(Card c) {
+		return assetStore.getCardGroup(c);
 	}
 }
