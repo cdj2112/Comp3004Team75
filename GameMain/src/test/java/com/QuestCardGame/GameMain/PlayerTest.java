a2e955b3268289e9889a79b87caa0221e88c62f8
Unknown <steffentsmith@gmail.com>
2018-02-25 12:32:17 -0500

    Add best possible hand test

--- GameMain/src/test/java/com/QuestCardGame/GameMain/PlayerTest.java
+++ GameMain/src/test/java/com/QuestCardGame/GameMain/PlayerTest.java
@@ -38,4 +38,28 @@ public class PlayerTest extends TestCase{
 		assert p.getHand().indexOf(f1) == 0;
 		assert p.getHand().indexOf(w1) == 1;
 	}
+	
+	/**
+	 * Shouldn't contain any foes
+	 * Shouldn't contain any duplicate weapons
+	 */
+	public void testBestPossibleHand() {
+		AdventureCard f1 = new Foe("TestFoe1", 6);
+		AdventureCard w1 = new Weapon("Dagger", 1);
+		AdventureCard w2 = new Weapon("Dagger", 1);
+		AdventureCard w3 = new Weapon("BattleAxe", 10);
+
+		Player p = new Player();
+		p.drawCard(w1);
+		p.drawCard(f1);
+		p.drawCard(w3);
+		
+		Hand h = p.getHand().getBestPossibleHand();
+		assert !h.contains(f1) && h.contains(w1) && h.contains(w3);
+		
+		p.drawCard(w2);
+		h = p.getHand().getBestPossibleHand();
+		assert !h.contains(f1) && h.contains(w1) && !h.contains(w2) && h.contains(w3);
+		
+	}
 }
