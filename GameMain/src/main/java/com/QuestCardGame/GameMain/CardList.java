d32044dbec759a6eab1f638410995b6d6450dcbe
Cooper Jones <cooperjones@cmail.carleton.ca>
2018-02-25 13:31:23 -0500

    card list will be reformatted into a static class

--- GameMain/src/main/java/com/QuestCardGame/GameMain/CardList.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/CardList.java
@@ -1,64 +1,40 @@
 package com.QuestCardGame.GameMain;
+
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 
 public class CardList {
-	private List<String> adventureList;
-	private List<String> attributeList;
-	private Map<String, Integer> storyList;
 	// Integer is stand for the amount of copies of a same card
 
-	public CardList() {
-		adventureList = new ArrayList<String>();
-		attributeList = new ArrayList<String>();
-		storyList = new HashMap<String, Integer>();
-		try {
-			generateList();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	/*
+	 * public CardList() { adventureList = new ArrayList<String>(); attributeList =
+	 * new ArrayList<String>(); storyList = new HashMap<String, Integer>(); try {
+	 * generateList(); } catch (FileNotFoundException e) { // TODO Auto-generated
+	 * catch block e.printStackTrace(); } // System.out.print(adventureList.get(1));
+	 * }
+	 * 
+	 * public List<String> getAdventureList() { return adventureList; } public
+	 * List<String> getAttributeList() { return attributeList; } public Map<String,
+	 * Integer> getStoryList() { return storyList; }
+	 */
+
+	public static void populateAdventureCards(Deck adventureDeck) throws FileNotFoundException {
+		Scanner weaponScanner = new Scanner(new File("./src/resources/WeaponList.txt"));
+		System.out.println("Open Weapon Scanner");
+		while (weaponScanner.hasNext()) {
+			String[] cardInfo = weaponScanner.next().split(";");
+			int amount = Integer.parseInt(cardInfo[1]);
+			System.out.println(Arrays.toString(cardInfo));
+			for (int i = 0; i < amount; i++) {
+				System.out.println(i);
+				Weapon w = new Weapon(cardInfo[0], Integer.parseInt(cardInfo[2]));
+				adventureDeck.addCard(w);
+			}
 		}
-		// System.out.print(adventureList.get(1));
-	}
-
-	public List<String> getAdventureList() {
-		return adventureList;
-	}
-	public List<String> getAttributeList() {
-		return attributeList;
-	}
-	public Map<String, Integer> getStoryList() {
-		return storyList;
-	}
-
-	private void generateList() throws FileNotFoundException {
-		// hardcode for now, may get from text file later
-		/* Adventure Card Initialization */
-		// Allies
-		Scanner s = new Scanner(new File("AdventureDeck.txt"));
-		Scanner s1 = new Scanner(new File("AdventureAttribute.txt"));
-		while (s.hasNext()) {
-			adventureList.add(s.next());
 		}
-		s.close();
-		while (s1.hasNext()) {
-			attributeList.add(s1.next());
-		}
-		s1.close();
-		/* Adventure Card Initialization finished */
-
-		/* Story Card Initialization */
-		// Quests
-		storyList.put("Search for the Holy Grail", 1);
-		storyList.put("Test of the Green Knight", 1);
-		storyList.put("Search for the Questing Beast", 1);
-		/* Strry Card Initialization finished */
-
 	}
-	public void updateList(int x) {
-		// not yet implenmented
 
 	}
 }
