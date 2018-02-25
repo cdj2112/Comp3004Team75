8699096fbae118672613edb4506cd0725b748435
Cooper Jones <cooperjones@cmail.carleton.ca>
2018-02-25 13:37:02 -0500

    remove print statments, replace _ with space in card name

--- GameMain/src/main/java/com/QuestCardGame/GameMain/CardList.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/CardList.java
@@ -22,27 +22,21 @@ public class CardList {
 
 	public static void populateAdventureCards(Deck adventureDeck) throws FileNotFoundException {
 		Scanner weaponScanner = new Scanner(new File("./src/resources/WeaponList.txt"));
-		System.out.println("Open Weapon Scanner");
 		while (weaponScanner.hasNext()) {
 			String[] cardInfo = weaponScanner.next().split(";");
 			int amount = Integer.parseInt(cardInfo[1]);
-			System.out.println(Arrays.toString(cardInfo));
 			for (int i = 0; i < amount; i++) {
-				System.out.println(i);
 				Weapon w = new Weapon(cardInfo[0], Integer.parseInt(cardInfo[2]));
 				adventureDeck.addCard(w);
 			}
 		}
 		
 		Scanner foeScanner = new Scanner(new File("./src/resources/FoeList.txt"));
-		System.out.println("Open Weapon Scanner");
 		while (foeScanner.hasNext()) {
 			String[] cardInfo = foeScanner.next().split(";");
-			System.out.println(Arrays.toString(cardInfo));
 			int amount = Integer.parseInt(cardInfo[1]);
 			for (int i = 0; i < amount; i++) {
-				System.out.println(i);
-				Foe f = new Foe(cardInfo[0], Integer.parseInt(cardInfo[2]), Integer.parseInt(cardInfo[3]));
+				Foe f = new Foe(cardInfo[0].replace('_', ' '), Integer.parseInt(cardInfo[2]), Integer.parseInt(cardInfo[3]));
 				adventureDeck.addCard(f);
 			}
 		}
