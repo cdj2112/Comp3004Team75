bb1c87b0125a8fb2085e9c069d2c94e0706b2a25
DishySun <dishysun@hotmail.com>
2018-02-27 14:38:24 -0500

    add Player logging

--- GameMain/src/main/java/com/QuestCardGame/GameMain/Player.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/Player.java
@@ -6,54 +6,57 @@ import org.apache.logging.log4j.LogManager;
 
 
 public class Player {
-		
+
 	private static final Logger logger = LogManager.getLogger(Player.class);
-	
+
 	private ArrayList<AdventureCard> hand;
 	private ArrayList<AdventureCard> play;
 	private int playerNumber;
 	private int numShields;
 	private int rank;
 	static int nextPlayerNumber = 1;
-	
+
 	Player(){
 		hand = new ArrayList<AdventureCard>();
 		play = new ArrayList<AdventureCard>();
 		playerNumber = nextPlayerNumber++;
 		numShields = 0;
 		rank = 5; //squire
-		
+
 		//disabled until log4j2.xml has been created
-		//logger.info("A new player has been created.");
+		logger.info("Player " + playerNumber + ": created");
 	}
-	
+
 	public void drawCard(AdventureCard c){
 		hand.add(c);
+		logger.info("Player " + playerNumber + ": DREW [" + c.getName() + "]");
 	}
-	
+
 	public void playCard(AdventureCard c) {
 		boolean removed = hand.remove(c);
 		if(removed) {
 			play.add(c);
+			logger.info("Player " + playerNumber + ": PLAYED [" + c.getName() + "]");
 		}
 	}
-	
+
 	public void useCard(Card c) {
 		hand.remove(c);
+		logger.info("Player " + playerNumber + ": USED [" + c.getName() + "]");
 	}
-	
+
 	public ArrayList<AdventureCard> getHand() {
 		return hand;
 	}
-	
+
 	public ArrayList<AdventureCard> getPlay() {
 		return play;
 	}
-	
+
 	public int getPlayerNumber() {
 		return playerNumber;
 	}
-	
+
 	public int getBattlePoints() {
 		int totalBattlePoints = rank + numShields;
 		for(AdventureCard c : play) {
@@ -61,13 +64,13 @@ public class Player {
 		}
 		return totalBattlePoints;
 	}
-	
+
 	public void addShields(int s) {
 		numShields += s;
 		//TODO:
 		//upgrade rank based on number of shields
 	}
-	
+
 	public int getNumShields() {
 		return numShields;
 	}
