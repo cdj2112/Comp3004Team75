429d68cb4f78ed4a37c7674c4f1d3c188e906b18
Unknown <dishysun@hotmail.com>
2018-03-02 03:48:28 -0500

    test updated

--- GameMain/src/test/java/com/QuestCardGame/GameMain/Scenario1Test.java
+++ GameMain/src/test/java/com/QuestCardGame/GameMain/Scenario1Test.java
@@ -15,14 +15,28 @@ public class Scenario1Test extends TestCase{
   	p1Test.add(boar);
   	Weapon sword = new Weapon("Sword",10);
   	p1Test.add(sword);
+		Weapon dagger = new Weapon("Dagger", 5);
+		p1Test.add(dagger);
   	testDeck.put(0, p1Test);
   	QuestCard boarHunt = new QuestCard("Boar Hunt", 2);
   	Game testGame = new Game(testDeck, boarHunt);
   	assert (testGame.getPlayer(0).getHand().contains((AdventureCard)saxons));
   	assert (testGame.getPlayer(0).getHand().contains((AdventureCard)boar));
   	assert (testGame.getPlayer(0).getHand().contains((AdventureCard)sword));
+  	assert (testGame.getPlayer(0).getHand().size() == 12);
+  	assert (testGame.getPlayer(1).getHand().size() == 12);
+  	assert (testGame.getPlayer(2).getHand().size() == 12);
+  	assert (testGame.getPlayer(3).getHand().size() == 12);
 
   	testGame.playTurn();
   	assert (testGame.getActiveQuest().getQuestName().equals(boarHunt.getName()));
+
+		testGame.acceptSponsor();
+		testGame.sponsorAddCardToStage(saxons, 0);
+		testGame.sponsorAddCardToStage(boar, 1);
+		testGame.sponsorAddCardToStage(dagger,1);
+		testGame.sponsorAddCardToStage(sword, 1);
+		assert (testGame.getPlayer(0).getHand().size() == 8);
+
 	}
 }
