35ad091db8c56cfe3948a218f35f8d112419b3b1
Unknown <steffentsmith@gmail.com>
2018-03-17 18:41:20 -0400

    Add AI One accept/decline quest decision.

--- GameMain/src/main/java/com/QuestCardGame/GameMain/AIStrategyOne.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/AIStrategyOne.java
@@ -11,10 +11,19 @@ public class AIStrategyOne extends AIPlayer {
 	}
 	
 	public boolean doIJoinTournament() {
-		//game.acceptDeclineTour(this, false);
-		return false;
+		boolean joinTournament = false;
+		if(aPlayerCanWinGame(true) || aPlayerCanEvolve(true)) {
+			joinTournament = true;
+			//game.acceptTournament();
+		}
+		else {
+			joinTournament = false;
+			//game.declineTournament();
+		}
+		return joinTournament;
 	}
 
+	//TODO need to add condition for another player in tournament to win/evolve to play best hand
 	public ArrayList<AdventureCard> playCardsForTournament() {
 		ArrayList<AdventureCard> cardsForTournament = new ArrayList<AdventureCard>();
 		if(canIWinGame()) {
@@ -28,8 +37,18 @@ public class AIStrategyOne extends AIPlayer {
 	}
 
 	public boolean doISponsorAQuest() {
-		game.declineSponsor();
-		return false;
+		int numStages = ((QuestCard)game.getActiveStoryCard()).getStages(); 
+		boolean hasCardsToSponsor = this.getHand().hasCardsToSponsorQuest(numStages);
+		
+		//has to have cards, and nobody else can win/evolve		
+		if(hasCardsToSponsor && !(aPlayerCanWinGame(false) || aPlayerCanEvolve(false))) { 
+			game.acceptSponsor();
+			return true;
+		}
+		else {
+			game.declineSponsor();
+			return false;
+		}	
 	}
 
 	public ArrayList<AdventureCard> createQuest() {
@@ -70,6 +89,36 @@ public class AIStrategyOne extends AIPlayer {
 		
 		//rank 2 is Champion Knight
 		return(rank == 2 && getShieldsNeeded() <= potentialReward);	
+	}
+	
+	private boolean aPlayerCanWinGame(boolean includingMyself) {
+		StoryCard storyCard = (StoryCard)game.getActiveStoryCard();
+		int potentialReward = storyCard.getShieldReward();
+		boolean aPlayerCanWin = false;
+		
+		for(int i = 0; i < game.getNumPlayers(); i++) {
+			Player p = game.getPlayer(i);
+			if(p == this && !includingMyself)
+				continue;
+			if(p.getRankName().equals("Champion Knight") && p.getShieldsNeeded() <= potentialReward)
+				aPlayerCanWin = true;				
+		}
+		return aPlayerCanWin;
+	}
+	
+	private boolean aPlayerCanEvolve(boolean includingMyself) {
+		StoryCard storyCard = (StoryCard)game.getActiveStoryCard();
+		int potentialReward = storyCard.getShieldReward();
+		boolean aPlayerCanEvolve = false;
+		
+		for(int i = 0; i < game.getNumPlayers(); i++) {
+			Player p = game.getPlayer(i);
+			if(p == this && !includingMyself)
+				continue;
+			if(p.getShieldsNeeded() <= potentialReward)
+				aPlayerCanEvolve = true;				
+		}
+		return aPlayerCanEvolve;
 	}
 
 }
