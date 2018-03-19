05abb9fa752ace6c16ce315674228e9654a39256
Cooper Jones <cooperjones@cmail.carleton.ca>
2018-03-19 18:03:03 -0400

    base of spring server
    DO NOT TOUCH

---
+++ GameMain/src/main/java/com/QuestCardGame/SpringServer/QuestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.QuestCardGame.SpringServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestSpringApplication.class, args);
+    }
+
+}
