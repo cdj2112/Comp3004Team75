46c1a67e7f0aecb09822c53bca20d2604b696379
Cooper Jones <cooperjones@cmail.carleton.ca>
2018-03-25 16:20:02 -0400

    add static map that match card to ids

--- GameMain/src/main/java/com/QuestCardGame/GameMain/Card.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/Card.java
@@ -1,14 +1,23 @@
 package com.QuestCardGame.GameMain;
 
+import java.util.HashMap;
+
 public abstract class Card {
 
 	private static int nextId = 0;
+	private static HashMap<Integer, Card> idMap = new HashMap<Integer, Card>();
+	
+	public static Card getCard(int i) {
+		return idMap.get(i);
+	}
+	
 	protected int id;
 	protected String cardName;
 
 	Card(String name) {
 		id = nextId++;
 		cardName = name;
+		idMap.put(id, this);
 	}
 
 	public int getId() {
