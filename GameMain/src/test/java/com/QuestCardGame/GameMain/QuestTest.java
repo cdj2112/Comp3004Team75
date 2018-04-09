965558125c8d5aec9d027749011487ca5a6701b5
Cooper Jones <cooperjones@cmail.carleton.ca>
2018-04-09 15:26:07 -0400

    fix quest tests

--- GameMain/src/test/java/com/QuestCardGame/GameMain/QuestTest.java
+++ GameMain/src/test/java/com/QuestCardGame/GameMain/QuestTest.java
@@ -8,7 +8,7 @@ public class QuestTest extends TestCase{
 	
 	public void testIsValidQuestIncreasingBattlePoints() {			
 		QuestCard c = new QuestCard("OneStageQuest", 2);
-		Quest quest = new Quest(c);
+		Quest quest = new Quest(c,0);
 		AdventureCard f1 = new Foe("TestFoe1", 1);
 		AdventureCard w1 = new Weapon("Dagger", 1);
 		AdventureCard f2 = new Foe("TestFoe2", 2);
@@ -23,7 +23,7 @@ public class QuestTest extends TestCase{
 	
 	public void testIsValidQuestDecreasingBattlePoints() {			
 		QuestCard c = new QuestCard("OneStageQuest", 2);
-		Quest quest = new Quest(c);
+		Quest quest = new Quest(c,0);
 		AdventureCard f1 = new Foe("TestFoe1", 10);
 		AdventureCard w1 = new Weapon("Dagger", 1);
 		AdventureCard f2 = new Foe("TestFoe2", 2);
@@ -43,7 +43,7 @@ public class QuestTest extends TestCase{
 	 */
 	public void testEvaluateStage() {
 		QuestCard c = new QuestCard("OneStageQuest", 2);
-		Quest quest = new Quest(c);
+		Quest quest = new Quest(c,0);
 		AdventureCard f1 = new Foe("TestFoe1", 6);
 		AdventureCard w1 = new Weapon("Dagger", 1);
 		AdventureCard f2 = new Foe("TestFoe2", 10);
