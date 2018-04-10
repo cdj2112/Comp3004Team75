b558ccd7b4d830969a1ee1fae8b1cdc7f597bc31
Cooper Jones <cooperjones@cmail.carleton.ca>
2018-04-10 02:17:41 -0400

    ai can execute events

--- GameMain/src/main/java/com/QuestCardGame/GameMain/AIPlayer.java
+++ GameMain/src/main/java/com/QuestCardGame/GameMain/AIPlayer.java
@@ -1,6 +1,11 @@
 package com.QuestCardGame.GameMain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ListIterator;
+import java.util.Set;
+
+import com.QuestCardGame.GameMain.AdventureCard.AdventureType;
 
 public abstract class AIPlayer implements AIPlayerStrategy{
 	
@@ -33,6 +38,11 @@ public abstract class AIPlayer implements AIPlayerStrategy{
 				return this.getCardsToDiscard();
 			case EVAL_TOUR:
 				return this.evalTour();
+			case EVENT_EXECUTE:
+				game.executeEvent();
+				return new ArrayList<AdventureCard>();
+			case EVENT_DISCARD:
+				return this.getSpecialDiscardCards();
 			case END_TURN_DISCARD:
 				return this.getCardsToDiscard();
 			case IDLE:
@@ -62,6 +72,33 @@ public abstract class AIPlayer implements AIPlayerStrategy{
 			cardsToDiscard.add(c);
 			numCardsToDiscard--;
 		}
+		
+		for(AdventureCard c : cardsToDiscard) {
+			game.playerDiscardAdventrueCard(this.player, c);
+		}
+		return cardsToDiscard;
+	}
+	
+	public ArrayList<AdventureCard> getSpecialDiscardCards(){
+		ArrayList<AdventureCard> cardsToDiscard = new ArrayList<AdventureCard>();
+		HashMap<AdventureType, Integer> map = game.getSpecialDiscard()[game.getPlayerIndex(player)];
+		
+		Hand playerHand = player.getHand();
+		playerHand.sortAscendingByBattlePoints();
+		
+		Set<AdventureType> types = map.keySet();
+		for(AdventureType t : types) {
+			int toDiscard = map.get(t);
+			ListIterator<AdventureCard> iter = playerHand.listIterator();
+			while(toDiscard > 0) {
+				AdventureCard c = iter.next();
+				if(c.getCardType() == t) {
+					cardsToDiscard.add(c);
+					toDiscard--;
+				}
+			}
+		}
+		
 		
 		for(AdventureCard c : cardsToDiscard) {
 			game.playerDiscardAdventrueCard(this.player, c);
